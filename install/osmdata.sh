f1f2be6be22b53c1893c11e08cd872459dae4382
psd <psd@whatfettle.com>
2014-10-03 14:57:54 +0100

    Impsom tweaks

--- install/osmdata.sh
+++ install/osmdata.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+#
+# install imposm to load OSM data into postgres
+#
+set -e -x
+
+# PBF_DIR=/europe
+# PBF=british-isles-latest.osm.pbf
+
+PBF_DIR=/europe/great-britain
+PBF=england-latest.osm.pbf
+
 sudo aptitude install -y build-essential \
     python-dev \
     protobuf-compiler \
@@ -11,11 +22,11 @@ sudo aptitude install -y build-essential \
 sudo apt-get install -y unzip
 sudo apt-get install -y git
 sudo apt-get install -y python-pip
+sudo apt-get install -y cython
 sudo pip install imposm
 
 # download OSM data
-PBF=british-isles-latest.osm.pbf
-wget -N http://download.geofabrik.de/europe/$PBF
+wget -N http://download.geofabrik.de$PBF_DIR/$PBF
 
 wget -N http://data.openstreetmapdata.com/simplified-land-polygons-complete-3857.zip
 unzip simplified-land-polygons-complete-3857.zip
