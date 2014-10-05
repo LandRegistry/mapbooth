3165f6c1eb2b77eac18e156bf44a1b053eac4b48
psd <psd@whatfettle.com>
2014-10-05 21:07:37 +0100

    Postgres user for imposm

--- install/imposm.sh
+++ install/imposm.sh
@@ -28,7 +28,7 @@ sudo apt-get install -y cython
 sudo pip install imposm
 
 # download OSM data
-wget -N http://download.geofabrik.de$PBF_DIR/$PBF
+wget -N http://download.geofabrik.de${PBF_DIR}/${PBF}
 
 # install OSM Bright map style
 [ ! -d osm-bright ] && git clone https://github.com/mapbox/osm-bright.git
@@ -37,4 +37,4 @@ wget -N http://download.geofabrik.de$PBF_DIR/$PBF
 imposm -U postgres -d osm -m osm-bright/imposm-mapping.py --read --merge-cache ${PBF}
 
 # write the data stored in the cache to the OSM database
-imposm -U npmap -d osm -m osm-bright/imposm-mapping.py --write --optimize --deploy-production-tables
+imposm -U postgres -d osm -m osm-bright/imposm-mapping.py --write --optimize --deploy-production-tables
