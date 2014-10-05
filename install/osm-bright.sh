c70e4028c2971276c21af220957e38478a16d4fe
psd <psd@whatfettle.com>
2014-10-05 21:06:46 +0100

    No longer freshens files

--- install/osm-bright.sh
+++ install/osm-bright.sh
@@ -14,16 +14,16 @@ cp ../etc/configure.py .
 
 # download and extract OSM shapefiles
 wget -N http://data.openstreetmapdata.com/simplified-land-polygons-complete-3857.zip
-unzip -f -o simplified-land-polygons-complete-3857.zip
+unzip -o simplified-land-polygons-complete-3857.zip
 
 wget -N http://data.openstreetmapdata.com/land-polygons-split-3857.zip
-unzip -f -o land-polygons-split-3857.zip
+unzip -o land-polygons-split-3857.zip
 
 # clear (any) existing  mapbox project
 sudo rm -rf /usr/share/mapbox/project/OSMBright
 
 # build osm-bright mapbox project
-sudo mapbox ./make.py
+./make.py
 
 # fixup ownership
 sudo chown -R mapbox:mapbox /usr/share/mapbox/project/OSMBright
